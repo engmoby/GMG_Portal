1d4205b8f6e03bc979d5aa24547a471a23f7c732
MohamedHussein <sub_Data_base@yahoo.com>
2017-07-09 17:47:42 +0200

    Code Revise Front models and Api Calls

--- GMG_Portal.Front/Controllers/HomeController.cs
+++ GMG_Portal.Front/Controllers/HomeController.cs
@@ -1,25 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Front.Models;
 using Newtonsoft.Json;
-using About = Front.Models.About;
-using HomeSlider = Front.Models.HomeSlider;
+using GMG_Portal.API.Models.SystemParameters;
 
 namespace Front.Controllers
 {
     public class HomeController : Controller
     {
         readonly HttpClient _client;
-        string url = "http://localhost:2798/SystemParameters/";
-
+       
+        string url = System.Configuration.ConfigurationManager.AppSettings["ServicesURL"] + "/SystemParameters/";
         public HomeController()
         {
-            //this._about = about;
-            //this._homeSlider = homeSlider;
+          
             _client = new HttpClient();
             _client.BaseAddress = new Uri(url);
             _client.DefaultRequestHeaders.Accept.Clear();
@@ -30,149 +29,118 @@ namespace Front.Controllers
         {
             string _homeSlider = url + "HomeSliders/GetAll";
             string _about = url + "Abouts/GetAll";
-            var _HotelFeatures = url + "Features/GetAll";
-            var _News = url + "News/GetAll";
-       
-            string _Hotel = url + "Hotels/GetAll";
-            var homeModels = new HomeModels();
-            var homesliders = new List<HomeSlider>();
-            var aboutList = new List<About>();
-            var hotelFeatures = new List<HotelFeatures>();
-            var newsList = new List<News>();
-            var hotels = new List<Hotel>();
+            string _HotelFeatures = url + "Features/GetAll";
+            string _News = url + "News/GetAll";
+            string _Hotels = url + "Hotels/GetAll";
 
-            await CallApi(_homeSlider, homesliders, homeModels);
-            await CallAbout(_about, aboutList, homeModels);
-            await CallFacilities(_HotelFeatures, hotelFeatures, homeModels);
-            await CallNews(_News, newsList, homeModels);
-          
-            await CallHotels(_Hotel, hotels, homeModels);
+
+
+            var homeModels = new HomeModels();
+        
+
+            await CallHomeSliders(_homeSlider, homeModels);
+            await CallAbout(_about, homeModels);
+            await CallFacilities(_HotelFeatures, homeModels);
+            await CallHotels(_Hotels, homeModels);
+            await CallNews(_News, homeModels);
+
+
             return View(homeModels);
 
         }
 
 
-        private async Task CallApi(string homeSlider, List<HomeSlider> homesliders, HomeModels homeModels)
+       // Home Partial Views Loading 
+        public ActionResult Slider()
         {
-            HttpResponseMessage responseMessage = await _client.GetAsync(homeSlider);
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
-                var home = JsonConvert.DeserializeObject<List<HomeSlider>>(responseData);
+            return Slider();
+        }
 
-                foreach (var forHomeSlider in home)
-                {
-                    homesliders.Add(new HomeSlider
-                    {
-                        DisplayValue = forHomeSlider.DisplayValue,
-                        Id = forHomeSlider.Id,
-                        DisplayValueDesc = forHomeSlider.DisplayValueDesc,
-                        Image = forHomeSlider.Image
-                    });
-                }
-                homeModels.HomeSliders = homesliders;
+        public ActionResult OurHotels()
+        {
+            return OurHotels();
+        }
 
-                // return View(homeModels);
-            }
+        public ActionResult AboutHome()
+        {
+            return AboutHome();
+        }
+
+        public ActionResult HotelFacilities()
+        {
+            return HotelFacilities();
+        }
+
+        public ActionResult OwnersHome()
+        {
+            return OwnersHome();
+        }
+
+        public ActionResult NewsHome()
+        {
+            return NewsHome();
         }
 
 
 
-        private async Task CallAbout(string _about, List<About> aboutList, HomeModels homeModels)
+
+         //Fill Models with data Retrieved
+
+        private async Task CallHomeSliders(string _homeSlider, HomeModels homeModels)
+        {
+            HttpResponseMessage responseMessageAbout = await _client.GetAsync(_homeSlider);
+            if (responseMessageAbout.IsSuccessStatusCode)
+            {
+                var responseData = responseMessageAbout.Content.ReadAsStringAsync().Result;
+                var homesliders = JsonConvert.DeserializeObject<List<HomeSlider>>(responseData);
+                homeModels.HomeSliders = homesliders;
+            }
+        }
+    
+        private async Task CallAbout(string _about, HomeModels homeModels)
         {
             HttpResponseMessage responseMessageAbout = await _client.GetAsync(_about);
             if (responseMessageAbout.IsSuccessStatusCode)
             {
                 var responseData = responseMessageAbout.Content.ReadAsStringAsync().Result;
                 var about = JsonConvert.DeserializeObject<List<About>>(responseData);
-
-
-                foreach (var forAbout in about)
-                {
-                    aboutList.Add(new About
-                    {
-                        DisplayValue = forAbout.DisplayValue,
-                        Id = forAbout.Id,
-                        DisplayValueDesc = forAbout.DisplayValueDesc,
-                        URL = forAbout.URL
-                    });
-                }
-                homeModels.About = aboutList;
+                homeModels.About = about;
             }
         }
-         
-        private async Task CallFacilities(string _HotelFeatures, List<HotelFeatures> hotelFeatureslist, HomeModels homeModels)
+
+        private async Task CallFacilities(string _HotelFeatures, HomeModels homeModels)
         {
             HttpResponseMessage responseMessageAbout = await _client.GetAsync(_HotelFeatures);
             if (responseMessageAbout.IsSuccessStatusCode)
             {
                 var responseData = responseMessageAbout.Content.ReadAsStringAsync().Result;
-                var featureslist = JsonConvert.DeserializeObject<List<HotelFeatures>>(responseData);
-
-
-                foreach (var forAbout in featureslist)
-                {
-                    hotelFeatureslist.Add(new HotelFeatures
-                    {
-                        DisplayValue = forAbout.DisplayValue,
-                        Id = forAbout.Id,
-                        DisplayValueDesc = forAbout.DisplayValueDesc,
-                        URL = forAbout.URL
-                    });
-                }
-                homeModels.Features = hotelFeatureslist;
+                var features = JsonConvert.DeserializeObject<List<Features>>(responseData);
+                homeModels.Features = features;
             }
         }
 
-
-        private async Task CallHotels(string _Hotel, List<Hotel> HotelList, HomeModels homeModels)
+        private async Task CallHotels(string _Hotels, HomeModels homeModels)
         {
-            HttpResponseMessage responseMessageAbout = await _client.GetAsync(_Hotel);
+            HttpResponseMessage responseMessageAbout = await _client.GetAsync(_Hotels);
             if (responseMessageAbout.IsSuccessStatusCode)
             {
                 var responseData = responseMessageAbout.Content.ReadAsStringAsync().Result;
-                var hotellist = JsonConvert.DeserializeObject<List<Hotel>>(responseData);
-
-
-                foreach (var forhotels in hotellist)
-                {
-                    HotelList.Add(new Hotel
-                    {
-                        DisplayValue = forhotels.DisplayValue,
-                        Id = forhotels.Id,
-                        DisplayValueDesc = forhotels.DisplayValueDesc,
-                        PriceStart = forhotels.PriceStart,
-                        Rate = forhotels.Rate ,
-                       Image = forhotels.Image,
-                        URL = forhotels.URL
-                    });
-                }
-                homeModels.Hotel  = HotelList;
+                var hotels = JsonConvert.DeserializeObject<List<Hotels>>(responseData);
+                homeModels.Hotels = hotels;
             }
         }
 
-        private async Task CallNews(string news, List<News> newslist, HomeModels homeModels)
+        private async Task CallNews(string _News, HomeModels homeModels)
         {
-            HttpResponseMessage responseMessageNews = await _client.GetAsync(news);
-            if (responseMessageNews.IsSuccessStatusCode)
+            HttpResponseMessage responseMessageAbout = await _client.GetAsync(_News);
+            if (responseMessageAbout.IsSuccessStatusCode)
             {
-                var responseData = responseMessageNews.Content.ReadAsStringAsync().Result;
-                var retunNewlist = JsonConvert.DeserializeObject<List<News>>(responseData);
-
-
-                foreach (var forAbout in retunNewlist)
-                {
-                    newslist.Add(new News
-                    {
-                        DisplayValue = forAbout.DisplayValue,
-                        Id = forAbout.Id,
-                        DisplayValueDesc = forAbout.DisplayValueDesc,
-                        URL = forAbout.URL
-                    });
-                }
-                homeModels.News = newslist;
+                var responseData = responseMessageAbout.Content.ReadAsStringAsync().Result;
+                var news = JsonConvert.DeserializeObject<List<News>>(responseData);
+                homeModels.News = news;
             }
         }
+
 
     }
 }
--- GMG_Portal.Front/Models/HomeModels.cs
+++ GMG_Portal.Front/Models/HomeModels.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using Front.Models;
+ using GMG_Portal.API.Models.SystemParameters;
+
 
 namespace Front.Models
 {
@@ -7,8 +9,8 @@ namespace Front.Models
     {
         public List<HomeSlider> HomeSliders { get; set; }
         public List<About> About { get; set; }
-        public List<HotelFeatures> Features { get; set; }
-        public List<Hotel> Hotel { get; set; }
+        public List<Features> Features { get; set; }
+        public List<Hotels> Hotels { get; set; }
         public List<News> News { get; set; }
     }
 }
