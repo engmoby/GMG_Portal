921207a070df85c77318c8d98d64dda002277b70
MohamedHussein <sub_Data_base@yahoo.com>
2017-07-11 15:15:00 +0200

    ..

--- GMG_Portal.Front/Controllers/OwnersController.cs
+++ GMG_Portal.Front/Controllers/OwnersController.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Front.Models;
+using GMG_Portal.API.Models.Hotels.Hotel;
+using Newtonsoft.Json;
+using GMG_Portal.API.Models.SystemParameters;
 namespace Front.Controllers
 {
     public class OwnersController : Controller
@@ -13,5 +21,10 @@ namespace Front.Controllers
         {
             return View();
         }
+
+
+     
+
+
     }
 }
