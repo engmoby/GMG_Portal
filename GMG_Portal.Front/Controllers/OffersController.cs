85c7a9f660066d04d192ad31179d90ff45bbdc58
MohamedHussein <sub_Data_base@yahoo.com>
2017-07-12 13:37:04 +0200

    Offers Initial

--- GMG_Portal.Front/Controllers/OffersController.cs
+++ GMG_Portal.Front/Controllers/OffersController.cs
@@ -1,17 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using GMG_Portal.API.Models.SystemParameters;
+
 
 namespace Front.Controllers
 {
     public class OffersController : Controller
     {
+
         // GET: Offers
-        public ActionResult Index()
+        readonly HttpClient _client;
+
+        string url = System.Configuration.ConfigurationManager.AppSettings["ServerIp"] + "/SystemParameters/";
+        public OffersController()
         {
-            return View();
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(url);
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+        // GET: Offers
+        public async Task<ActionResult> Index()
+        {
+            string Offers = url + "Offers/GetAll";
+            var OffersModels = new List<Offer>();
+
+            if (Offers == null) throw new ArgumentNullException(nameof(Offers));
+            HttpResponseMessage responseMessageApi = await _client.GetAsync(Offers);
+            if (responseMessageApi.IsSuccessStatusCode)
+            {
+                var responseData = responseMessageApi.Content.ReadAsStringAsync().Result;
+                var OffersList = JsonConvert.DeserializeObject<List<Offer>>(responseData);
+                OffersModels = OffersList;
+            }
+            return View(OffersModels);
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
