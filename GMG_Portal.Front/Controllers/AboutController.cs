30148ad8fd7d65249cf2e550c8d034b61f36b702
MohamedHussein <sub_Data_base@yahoo.com>
2017-07-12 17:14:26 +0200

    contact control and view

--- GMG_Portal.Front/Controllers/AboutController.cs
+++ GMG_Portal.Front/Controllers/AboutController.cs
@@ -1,17 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using GMG_Portal.API.Models.SystemParameters;
 
 namespace Front.Controllers
 {
     public class AboutController : Controller
     {
-        // GET: About
-        public ActionResult Index()
+        // GET: About Content
+        readonly HttpClient _client;
+
+        string url = System.Configuration.ConfigurationManager.AppSettings["ServerIp"] + "/SystemParameters/";
+        public AboutController()
         {
-            return View();
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(url);
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+        // GET: About Content
+        public async Task<ActionResult> Index()
+        {
+
+          
+
+
+
+            string About = url + "About/GetAll";
+            var AboutModels = new List<About>();
+
+            if (About == null) throw new ArgumentNullException(nameof(About));
+            HttpResponseMessage responseMessageApi = await _client.GetAsync(About);
+            if (responseMessageApi.IsSuccessStatusCode)
+            {
+                var responseData = responseMessageApi.Content.ReadAsStringAsync().Result;
+                var AboutList = JsonConvert.DeserializeObject<List<About>>(responseData);
+                AboutModels = AboutList;
+            }
+            return View(AboutModels);
+
+        }
+
+
+
+
+    
+
+
+
     }
 }
--- GMG_Portal.Front/Controllers/ContactController.cs
+++ GMG_Portal.Front/Controllers/ContactController.cs
@@ -1,17 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using GMG_Portal.API.Models.SystemParameters;
+using GMG_Portal.API.Models.SystemParameters.ContactUs;
 
 namespace Front.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
-        public ActionResult Index()
+
+
+
+        // GET: About Content
+        readonly HttpClient _client;
+
+        string url = System.Configuration.ConfigurationManager.AppSettings["ServerIp"] + "/SystemParameters/";
+        public ContactController()
         {
-            return View();
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(url);
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+        // GET: About Content
+        public async Task<ActionResult> Index()
+        {
+
+
+
+
+
+            string contactUs = url + "ContactUs/GetAll";
+            var contactUsModels = new List<ContactUs>();
+
+            if (contactUs == null) throw new ArgumentNullException(nameof(contactUs));
+            HttpResponseMessage responseMessageApi = await _client.GetAsync(contactUs);
+            if (responseMessageApi.IsSuccessStatusCode)
+            {
+                var responseData = responseMessageApi.Content.ReadAsStringAsync().Result;
+                var contactUsList = JsonConvert.DeserializeObject<List<ContactUs>>(responseData);
+                contactUsModels = contactUsList;
+            }
+            return View(contactUsModels);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
