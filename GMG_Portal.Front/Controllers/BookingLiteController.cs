aa4bd68516fa5cf044ff67212ab19a36292e7967
MohamedHussein <sub_Data_base@yahoo.com>
2017-07-16 14:51:13 +0200

    bookinglite

--- GMG_Portal.Front/Controllers/BookingLiteController.cs
+++ GMG_Portal.Front/Controllers/BookingLiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GMG_Portal.API.Models.Hotels.Reservation;
 
 namespace Front.Controllers
 {
@@ -12,6 +13,30 @@ namespace Front.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Reservation collection)
+        {
+            try
+            {
+
+                //Get the Querystrings Sent from Index 
+                var checkIn = Request.QueryString["CheckIn"];
+                var checkOut = Request.QueryString["CheckOut"];
+                var adult = Request.QueryString["Adult"];
+                var child = Request.QueryString["Child"];
+
+
+
+                var dd = collection.FirstName;
+                // TODO: Add insert logic here
+             
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
