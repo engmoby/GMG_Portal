8c5ed281733d3d5381f082514faf941c11bab909
MohamedHussein <sub_Data_base@yahoo.com>
2017-07-19 18:08:47 +0200

    About , news , vision , mission

---
+++ GMG_Portal.UI/Scripts/app/services/About.js
@@ -0,0 +1,15 @@
+﻿provide.service('AboutApi', function ($http) {
+    var apiUrl = document.querySelector('#HServicesURL').value;
+    this.GetAll = function () {
+        return $http.get(apiUrl + '/SystemParameters/About/GetAllWithDeleted');
+    }
+
+    this.Save = function (vision) {
+        return $http({
+            url: apiUrl + '/SystemParameters/About/Save',
+            method: 'POST',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(vision)
+        });
+    }
+});
---
+++ GMG_Portal.UI/Scripts/app/services/News.js
@@ -0,0 +1,15 @@
+﻿provide.service('NewsApi', function ($http) {
+    var apiUrl = document.querySelector('#HServicesURL').value;
+    this.GetAll = function () {
+        return $http.get(apiUrl + '/SystemParameters/News/GetAllWithDeleted');
+    }
+
+    this.Save = function (news) {
+        return $http({
+            url: apiUrl + '/SystemParameters/News/Save',
+            method: 'POST',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(news)
+        });
+    }
+});
