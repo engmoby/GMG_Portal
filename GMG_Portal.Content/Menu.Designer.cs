95bafc06129ef33bfbf84873f2a63ef9bd9fd546
Muhammadabdo <eng.mohammadabdo@gmail.com>
2017-07-19 11:39:07 +0200

    resolve vission& mission routing and create owners controller

--- GMG_Portal.Content/Menu.Designer.cs
+++ GMG_Portal.Content/Menu.Designer.cs
@@ -61,6 +61,15 @@ namespace GMG_Portal.Content {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to About.
+        /// </summary>
+        public static string About {
+            get {
+                return ResourceManager.GetString("About", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Account Type.
         /// </summary>
         public static string AccountType {
