44fcb1b7ce224c7f980b2f9b3baffbbbeddad203
Muhammadabdo <eng.mohammadabdo@gmail.com>
2017-07-26 14:21:44 +0200

    backend finish the newsletter module

---
+++ GMG_Portal.UI/Scripts/app/services/Newsletter.js
@@ -0,0 +1,16 @@
+﻿provide.service('NewslettersApi', function ($http) {
+    var apiUrl = document.querySelector('#HServicesURL').value;
+    this.GetAll = function () {
+        return $http.get(apiUrl + '/SystemParameters/Newsletter/GetAllWithDeleted');
+    }
+
+    this.Save = function (newsletter) {
+        return $http({
+            url: apiUrl + '/SystemParameters/Newsletter/Save',
+            method: 'POST',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(newsletter)
+        });
+    }
+     
+});
