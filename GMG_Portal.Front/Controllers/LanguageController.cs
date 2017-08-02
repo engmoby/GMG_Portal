be9bbc4249d99cfa4710af47624400ec805adf8f
MohamedHussein <sub_Data_base@yahoo.com>
2017-08-02 12:28:31 +0200

    Resource Update

---
+++ GMG_Portal.Front/Controllers/LanguageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Front.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Lang
+        public ActionResult ChangeLanguage(string selectedLanguage)
+        {
+            if (selectedLanguage != null)
+            {
+                Thread.CurrentThread.CurrentCulture =
+                    CultureInfo.CreateSpecificCulture(selectedLanguage);
+                Thread.CurrentThread.CurrentUICulture =
+                    new CultureInfo(selectedLanguage);
+                var cookie = new HttpCookie("Language");
+                cookie.Value = selectedLanguage;
+                Response.Cookies.Add(cookie);
+            }
+            //   return RedirectToAction("Index", "Home");
+            return Redirect(Request.UrlReferrer.ToString());
+
+        }
+    }
+}
---
+++ GMG_Portal.Front/Models/MultiLang.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AutoMapper.XpressionMapper;
+
+namespace Front.Models
+{
+    public class MultiLang
+    {
+        [Display(Name = "Home", ResourceType = typeof(Resource))]
+        public string Home { get; set; }
+
+        [Display(Name = "About", ResourceType = typeof(Resource))]
+        public string Hotels { get; set; }
+    }
+}
