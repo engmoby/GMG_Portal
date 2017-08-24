63754efade70b0af7268cad4f557f859852dfc8c
MohamedHussein <sub_Data_base@yahoo.com>
2017-08-24 17:40:33 +0200

    Contact Us Validation Multi

--- GMG_Portal.Front/Helpers/Common.cs
+++ GMG_Portal.Front/Helpers/Common.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace Front.Helpers
 {
     public class Common
     {
         public static string CurrentLang = "en";
     }
+   
+
 }
